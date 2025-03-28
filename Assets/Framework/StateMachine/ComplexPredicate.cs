2247f3d81ea5f95631e48a851743f9f5d048349e
Eunseok <79277772+Eunseok@users.noreply.github.com>
2025-03-28 15:18:06 +0900

    [Feat] 플레이어 공격

---
+++ Assets/Framework/StateMachine/ComplexPredicate.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Timelesss
+{
+    public class ComplexPredicate : IPredicate
+    {
+        private readonly IEnumerable<IPredicate> predicates;
+
+        public ComplexPredicate(IEnumerable<IPredicate> predicates)
+        {
+            this.predicates = predicates ?? throw new ArgumentNullException(nameof(predicates));
+        }
+
+        public bool Evaluate()
+        {
+            return predicates.All(predicate => predicate.Evaluate());
+        }
+    }
+}
