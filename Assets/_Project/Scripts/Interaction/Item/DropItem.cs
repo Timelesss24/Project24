11666d5f7a1bc42c2b05122f0abda9b4670694b7
kdh5790 <109937570+kdh5790@users.noreply.github.com>
2025-03-28 11:02:50 +0900

    [Feat] 아이템 데이터, 스포너 추가
    * [Feat] 아이템 SO 기본적인 데이터 추가
    
    * [Feat] 아이템 스포너 추가
    
    * [Chore] 스크립트 인코딩 방식 변경
    
    * [Test] 테스트 아이템 추가
    
    * [Chore] 아이템스포너 프리팹화

---
+++ Assets/_Project/Scripts/Interaction/Item/DropItem.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+namespace Timelesss
+{
+    public class DropItem : InteractableBase
+    {
+        [SerializeField] private ItemData itemData;
+        public ItemData ItemData {  get { return itemData; } }
+
+        public override string InteractionName { get; } = "획득하기";
+
+        private ItemSpawner spawner;
+
+        public override void Interact()
+        {
+            Debug.Log($"{itemData.ItemName} 획득");
+
+            // 아이템 줍기 애니메이션
+
+            // UI 프롬프트 띄워주기 (XX 를 획득했습니다.)
+
+            // inventory.AddItem(item);
+
+
+            spawner.ReturnItem(this);
+        }
+
+        public void Initialize(ItemData data)
+        {
+            itemData = data;
+
+            if( spawner == null )
+            {
+                spawner = FindObjectOfType<ItemSpawner>();
+
+                if( spawner == null )
+                {
+                    Debug.LogError("현재 Scene에서 ItemSpawner를 찾을 수 없습니다.");
+                }
+            }
+        }
+    }
+}
---
+++ Assets/_Project/Scripts/Interaction/Item/ItemBase.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Timelesss
+{
+    public abstract class ItemBase : MonoBehaviour, IEntity
+    {
+        [SerializeField] protected ItemData itemData {  get; private set; }
+    }
+}
