d2dc4e2a4ea1c694923d000c1b022f6f9277d643
kdh5790 <k_0106@naver.com>
2025-03-30 14:27:56 +0900

    [Refactor] 아이템 데이터 수정

--- Assets/_Project/Scripts/ScriptableObjects/Item/ConsumableItemData.cs
+++ Assets/_Project/Scripts/ScriptableObjects/Item/ConsumableItemData.cs
@@ -3,26 +3,24 @@ using UnityEngine;
 
 namespace Timelesss
 {
+    public enum PotionType
+    {
+        HP,
+        Stamina
+    }
+
     [CreateAssetMenu(fileName = "New Consumable Item Data", menuName = "Item/Consumable Item Data")]
     public class ConsumableItemData : ItemData
     {
-        public PotionEffect effect;
-
         public float effectValue;   // 효과 값 (예: 회복량, 증가량 등)
         public float duration;      // 지속 시간 (0이면 즉시 효과)
+        public PotionType type;
 
         public override string ItemType => "소비 아이템";
 
         public void Use()
         {
-            if (effect != null)
-            {
-                effect.ApplyEffect(effectValue, duration);
-            }
-            else
-            {
-                Debug.LogError("포션 효과가 할당되지 않았습니다.");
-            }
+            //playerStatus.useitem(effectValue, duration, type);
         }
     }
 }
