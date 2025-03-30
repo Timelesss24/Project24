787d70a178cf6cdea5c0f300616a05ca421efcb2
song7446 <songwon4529@naver.com>
2025-03-30 16:11:50 +0900

    [Feat] 아이템 설명 추가

--- Assets/_Project/Scripts/UI/InventoryUI/ItemSlot.cs
+++ Assets/_Project/Scripts/UI/InventoryUI/ItemSlot.cs
@@ -1,13 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 namespace Timelesss
 {
-    public class ItemSlot : MonoBehaviour
+    public class ItemSlot : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
     {
-        public Image itemIcon;
         public ItemData itemData;
+        [SerializeField] GameObject itemDescriptionObject;
+        [SerializeField] private Image itemIcon;
+
+        private void Start()
+        {
+            itemIcon.sprite = itemData.ItemIcon;
+            itemDescriptionObject.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = itemData.ItemName;
+            itemDescriptionObject.transform.GetChild(1).GetComponent<TextMeshProUGUI>().text = itemData.ItemDescription;
+        }
+
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            itemDescriptionObject.SetActive(true);
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            itemDescriptionObject.SetActive(false);
+        }
     }
 }
