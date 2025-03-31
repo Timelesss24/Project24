1d40544b7e15b16a49105ae936d7ed6a6919236b
kdh5790 <109937570+kdh5790@users.noreply.github.com>
2025-03-31 12:01:17 +0900

    [Feat] 문 상호작용 추가
    * [Test] 문 버그 테스트
    
    * [Feat] 문 상호작용 추가
    
    * [Feat] 더블도어 상호작용 추가
    
    * [Feat] 문 열기 애니메이션 추가
    
    * [Fix] 문 열기 애니메이션 프레임 조정

---
+++ Assets/_Project/Scripts/Interaction/Door.cs
@@ -0,0 +1,117 @@
+using Managers;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Timelesss
+{
+    public class Door : InteractableBase
+    {
+        enum State { Single, Double }
+
+
+        public override string InteractionName => "¿­±â";
+
+        [SerializeField] private State doorState;
+
+        [SerializeField] private Transform[] doorTransforms;
+
+        private bool isOpen;
+
+        public override void Interact()
+        {
+            base.Interact();
+
+            RotateDoor();
+        }
+
+        private void RotateDoor()
+        {
+            if (doorState == State.Single)
+            {
+                StartCoroutine(RotateSingleDoor());
+            }
+            else if (doorState == State.Double)
+            {
+                StartCoroutine(RotateDoubleDoor());
+            }
+        }
+
+        private IEnumerator RotateSingleDoor()
+        {
+            if (isOpen) yield break;
+
+            isOpen = true;
+            interactionText.text = string.Empty;
+
+            float rotationTime = Clip.averageDuration;
+            float time = 0f;
+
+            float initialRotationY = transform.rotation.eulerAngles.y;
+            float targetRotationY = transform.rotation.eulerAngles.y - 90f;
+
+            while (time < rotationTime)
+            {
+                float newRotationY = Mathf.Lerp(initialRotationY, targetRotationY, time / rotationTime);
+
+                transform.rotation = Quaternion.Euler(0, newRotationY, 0);
+
+                time += Time.deltaTime;
+                yield return null;
+            }
+
+            InteractionManager.Instance.EndInteraction();
+
+            transform.rotation = Quaternion.Euler(0, targetRotationY, 0);
+        }
+
+        private IEnumerator RotateDoubleDoor()
+        {
+            if (isOpen) yield break;
+
+            isOpen = true;
+            interactionText.text = string.Empty;
+
+            float rotationTime = Clip.averageDuration; 
+            float time = 0f;
+
+            List<Quaternion> initialRotations = new List<Quaternion>();
+            List<Quaternion> targetRotations = new List<Quaternion>();
+
+            for (int i = 0; i < doorTransforms.Length; i++)
+            {
+                initialRotations.Add(doorTransforms[i].localRotation);
+                targetRotations.Add(Quaternion.Euler(0, i == 0 ? -90f : 90f, 0));
+            }
+
+            while (time < rotationTime)
+            {
+                for (int i = 0; i < doorTransforms.Length; i++)
+                {
+                    doorTransforms[i].localRotation = Quaternion.Lerp(
+                        initialRotations[i],
+                        targetRotations[i],
+                        time / rotationTime
+                    );
+                }
+
+                time += Time.deltaTime;
+                yield return null;
+            }
+
+            for (int i = 0; i < doorTransforms.Length; i++)
+            {
+                doorTransforms[i].localRotation = targetRotations[i];
+            }
+
+            InteractionManager.Instance.EndInteraction();
+        }
+
+        protected override void OnTriggerEnter(Collider other)
+        {
+            if (isOpen) return;
+
+            base.OnTriggerEnter(other);
+        }
+    }
+}
