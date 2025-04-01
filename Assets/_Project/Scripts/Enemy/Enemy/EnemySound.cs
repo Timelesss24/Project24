bb9d78d820dd8c3552081cc892fd6993d2687cd0
YataGarasu8 <zmffldjdnld12@naver.com>
2025-04-01 19:10:45 +0900

    [Feat]보스 몬스터 음성 추가

--- Assets/_Project/Scripts/Enemy/Enemy/EnemySound.cs
+++ Assets/_Project/Scripts/Enemy/Enemy/EnemySound.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,16 +6,36 @@ namespace Timelesss
 {
     public class EnemySound : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
-        {
-        
-        }
+        public AudioClip[] audioClips;
+        private AudioSource audioSource;
 
-        // Update is called once per frame
-        void Update()
+        private void Start()
         {
-        
+            audioSource = GetComponent<AudioSource>();
+        }
+        public void IdleSound()
+        {
+            audioSource.PlayOneShot(audioClips[0]);
+        }
+        public void WalkSound()
+        {
+            audioSource.PlayOneShot(audioClips[1]);
+        }
+        public void DieSound()
+        {
+            audioSource.PlayOneShot(audioClips[2]);
+        }
+        public void AttackFirstSound()
+        {
+            audioSource.PlayOneShot(audioClips[3]);
+        }
+        public void AttackSecondSound()
+        {
+            audioSource.PlayOneShot(audioClips[4]);
+        }
+        public void AttackThirdSound()
+        {
+            audioSource.PlayOneShot(audioClips[5]);
         }
     }
 }
