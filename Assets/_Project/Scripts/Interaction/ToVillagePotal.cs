116185b384cf7458a9eb761fe79529edf9a4e5b3
kdh5790 <k_0106@naver.com>
2025-04-01 13:04:43 +0900

    [Test] 머지 전 테스트

---
+++ Assets/_Project/Scripts/Interaction/ToVillagePotal.cs
@@ -0,0 +1,28 @@
+﻿using Managers;
+using UnityEngine;
+
+
+namespace Timelesss
+{
+    public class ToVillagePotal : InteractableBase
+    {
+        public override string InteractionName { get; } = "돌아가기";
+
+        [SerializeField] private string targetSceneName;
+
+        public override void Interact()
+        {
+            base.Interact();
+            // 입장 확인 팝업 띄워주기
+            Debug.Log($"{targetSceneName} 마을 돌아가기 입장");
+
+
+            var popup = UIManager.Instance.ShowPopup<ConfirmPopup>();
+            popup.InitailizePoup("마을로 돌아가시겠습니까?", () => {
+                InteractionManager.Instance.EndInteraction();
+                GameStateManager.Instance.SetGameState(GameStateManager.GameState.Village);
+            }, () => InteractionManager.Instance.EndInteraction());
+
+        }
+    }
+}
