9a38e6a9d8462fb5f8d8d34b15cc5279d2e4f40a
YataGarasu8 <zmffldjdnld12@naver.com>
2025-04-02 09:26:13 +0900

    [Feat]드롭 스크립트 추가

--- Assets/_Project/Scripts/Enemy/BossEnemy/BossEnemy.cs
+++ Assets/_Project/Scripts/Enemy/BossEnemy/BossEnemy.cs
@@ -1,6 +1,4 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
-using KBCore.Refs;
 using UnityEngine;
 using UnityEngine.AI;
 using Utilities;
---
+++ Assets/_Project/Scripts/Enemy/Enemy/EnemyDrop.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Timelesss
+{
+    public class EnemyDrop : MonoBehaviour
+    {
+        //[Header("드롭 아이템")]
+        //[SerializeField]pu
+    }
+}
