a22b653c46c3c0b751f1d509cb1011ecc0f6f63d
YataGarasu8 <zmffldjdnld12@naver.com>
2025-04-02 11:13:49 +0900

    [Feat]에너미 드롭

--- Assets/_Project/Scripts/Enemy/Enemy/EnemyDrop.cs
+++ Assets/_Project/Scripts/Enemy/Enemy/EnemyDrop.cs
@@ -6,7 +6,34 @@ namespace Timelesss
 {
     public class EnemyDrop : MonoBehaviour
     {
-        //[Header("드롭 아이템")]
-        //[SerializeField]pu
+        [System.Serializable]
+        public class EnemyDropItem
+        {
+            public ItemDetails itemDetails;
+            public float probability; // 0~1 사이의 확률 (예: 0.1은 10%)
+        }
+
+        [SerializeField] private DropItem dropItemPrefab;
+        [SerializeField] public List<EnemyDropItem> itemDataList = new List<EnemyDropItem>();
+
+        
+        public ItemDetails RandomItemDetails()
+        {
+            float total = 0f;
+            foreach (var item in itemDataList)
+                total += item.probability;
+
+            float randomPoint = Random.value * total;
+
+            float current = 0f;
+            foreach (var item in itemDataList)
+            {
+                current += item.probability;
+                if (randomPoint <= current)
+                    return item.itemDetails;
+            }
+
+            return null;
+        }
     }
 }
